cf40eec13b1f220255ca1632bea104c2e6172109
ian <ianmwendamicheni@gmail.com>
2023-12-12 07:47:09 +0300

    first commit

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+# lib/models.py
+from sqlalchemy import ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -15,6 +16,7 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
+    freebies = relationship('Freebie', back_populates='company')
 
     def __repr__(self):
         return f'<Company {self.name}>'
@@ -23,7 +25,25 @@ class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+    freebies = relationship('Freebie', back_populates='dev')
+    given_freebies = relationship('Freebie', back_populates='giver', foreign_keys='Freebie.giver_id')
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev = relationship('Dev', back_populates='freebies')
+    company = relationship('Company', back_populates='freebies')
+    giver_id = Column(Integer(), ForeignKey('devs.id'))
+    giver = relationship('Dev', back_populates='given_freebies', foreign_keys=[giver_id])
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Company, Dev, Freebie
+
+engine = create_engine('sqlite:///freebies.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create sample data
+company1 = Company(name="TechCorp", founding_year=2000)
+company2 = Company(name="CodeHub", founding_year=2010)
+dev1 = Dev(name="John Doe")
+dev2 = Dev(name="Jane Doe")
+
+freebie1 = Freebie(item_name="T-shirt", value=20, company=company1, dev=dev1)
+freebie2 = Freebie(item_name="Sticker", value=5, company=company2, dev=dev1)
+freebie3 = Freebie(item_name="Laptop", value=1000, company=company1, dev=dev2)
+
+# Add to session
+session.add_all([company1, company2, dev1, dev2, freebie1, freebie2, freebie3])
+session.commit()
